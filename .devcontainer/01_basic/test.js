b876a79730e0386e4b0d49b4e7e871e93dbccee6
Anshi180803 <140632992+Anshi180803@users.noreply.github.com>
2025-06-21 09:11:54 +0000

    code

--- .devcontainer/01_basic/test.js
+++ .devcontainer/01_basic/test.js
@@ -1,1 +1,11 @@
-console.log("avni")
+const account_id = "12123"
+let account_email = "avni000@gmail.com"
+var account_password ="34567"
+account_city = "bathinda"
+
+// const can not be changed
+
+/*console.log used for print single liners and console.table print table form */
+ 
+
+console.table([account_id,account_email,account_password,account_city])
